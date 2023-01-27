d4d83afdb98fd903c12a1a6024401b48648a9b5c
Petr Gregor <petr.gregor@email.cz>
2023-01-27 19:45:28 +0100

    Models - updates.

--- viewer/models.py
+++ viewer/models.py
@@ -5,75 +5,120 @@ from django.contrib.auth.models import User
 
 # Create your models here
 class Genre(Model):
-    name = CharField(max_length=16)
+    name = CharField(max_length=16, null=False, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class AgeRestriction(Model):
-    name = CharField(max_length=16)
+    name = CharField(max_length=16, null=False, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Country(Model):
-    name = CharField(max_length=32)
+    name = CharField(max_length=32, null=False, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
 
 
 class AwardCategory(Model):
-    name = CharField(max_length=32)
+    name = CharField(max_length=32, null=False, unique=True)
 
-
-class Staff(Model):
-    name = CharField(max_length=16)
-    surname = CharField(max_length=16)
-    country = ForeignKey(Country, on_delete=SET_NULL)
-    date_of_birth = DateField()
-    death_date = DateField()
-    biography = TextField()
-    awards = ManyToMany(Award)
+    def __str__(self):
+        return self.name
 
 
 class Image(Model):
-    path = CharField(max_length=64)
-    description = CharField(max_length=128)
+    path = CharField(max_length=64, null=False)
+    description = CharField(max_length=128, null=True)
+
+    def __str__(self):
+        return self.description[:30]
+
 
 
 class Movie(Model):
-    title_orig = CharField(max_length=64)
-    title_cz = CharField(max_length=64)
-    title_sk = CharField(max_length=64)
+    title_orig = CharField(max_length=64, null=False)
+    title_cz = CharField(max_length=64, null=True)
+    title_sk = CharField(max_length=64, null=True)
     country = ForeignKey(Country, null=True, on_delete=SET_NULL)
-    genre = ForeignKey(Genre, on_delete=SET_NULL)
-    released = DateField()
-    directors = ManyToManyField(Staff)
-    actors = ManyToManyField(Staff)
-    length = PositiveIntegerField()
-    description = TextField()
-    expanses = PositiveIntegerField()
-    earnings = PositiveIntegerField()
-    age_restriction = ForeignKey(AgeRestriction, on_delete=SET_NULL)
+    genre = ForeignKey(Genre, null=True, on_delete=SET_NULL)
+    released = DateField(null=False)
+    #directors = ManyToManyField(Staff)
+    #actors = ManyToManyField(Staff)
+    length = PositiveIntegerField(null=True)
+    description = TextField(null=True)
+    expanses = PositiveIntegerField(null=True)
+    earnings = PositiveIntegerField(null=True)
+    age_restriction = ForeignKey(AgeRestriction, null=True, on_delete=SET_NULL)
     images = ManyToManyField(Image)
-    trailer = CharField(max_length=256)
-    price = DecimalField(max_digits=6, decimal_places=2)
-    link = CharField(max_length=256)
+    trailer = CharField(max_length=256, null=True)
+    price = DecimalField(max_digits=6, decimal_places=2, null=True)
+    link = CharField(max_length=256, null=True)
     created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['title_orig']
 
     def __str__(self):
         return self.title + " (" + str(self.released) + ")"
 
 
 class Rating(Model):
-    movie = ForeignKey(Movie, on_delete=CASCADE)
-    user = ForeignKey(User, on_delete=SET_NULL)
-    rating = PositiveSmallIntegerField()
+    movie = ForeignKey(Movie, null=False, on_delete=CASCADE)
+    user = ForeignKey(User, null=True, on_delete=SET_NULL)
+    rating = PositiveSmallIntegerField(null=False)
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['movie', 'created']
+
+    def __str__(self):
+        return self.movie.title_orig + " " + self.user.name + " " + self.rating
 
 
 class Award(Model):
-    name = CharField(max_length=32)
-    year = IntegerField()
-    movie = ForeignKey(Movie, on_delete=SET_NULL)
-    category = ForeignKey(AwardCategory, on_delete=SET_NULL)
+    name = CharField(max_length=32, null=False)
+    year = IntegerField(null=False)
+    movie = ForeignKey(Movie, null=True, on_delete=SET_NULL)
+    category = ForeignKey(AwardCategory, null=True, on_delete=SET_NULL)
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-year', 'category']
+
+    def __str__(self):
+        return self.name + " (" + self.year + ") " + self.movie.title_orig
+
+
+class Staff(Model):
+    name = CharField(max_length=16, null=True)
+    surname = CharField(max_length=16, null=True)
+    artist_name = CharField(max_length=16, null=True)
+    country = ForeignKey(Country, null=True, on_delete=SET_NULL)
+    date_of_birth = DateField(null=True)
+    death_date = DateField(null=True)
+    biography = TextField(null=True)
+    awards = ManyToManyField(Award)
+    directing = ManyToManyField(Movie, related_name='directing_movie')
+    acting = ManyToManyField(Movie, related_name='acting_in_movie')
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['surname', 'artist_name', 'name']
+
+    def __str__(self):
+        return self.name + " " + self.surname + " " + self.artist_name
+
 
